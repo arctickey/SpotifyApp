c99a94c3bdf9ed6d0c17a0105e3feabe40b7ee40
arctickey <filipchrzuszcz1@gmail.com>
2020-01-09 11:49:45 +0100

    Update

--- App/app.R
+++ App/app.R
@@ -1,6 +1,4 @@
-
 library(shiny)
-
 library(tidyverse)
 library(rjson)
 library(lubridate)
@@ -12,12 +10,27 @@ data$endTime <- as.character(data$endTime)
 data$endTime <- fast_strptime(data$endTime, "%Y-%m-%d %H:%M:%S",tz="UTC")
 data$endTime <- as.POSIXct(data$endTime)
 x <- data %>% group_by(artistName) %>% summarise(time = sum(msPlayed)) %>% arrange(desc(time)) %>% slice(1:10)
+choices <- unique(x$artistName)
+
+
 ui <- fluidPage(
     
-    titlePanel("Old Faithful Geyser Data"),
-    
+  
     sidebarLayout(
+      
+        
         sidebarPanel(width=4,
+                     titlePanel("Time played"),
+                     
+                     checkboxInput("checkbox", label = "Plot by artist", value = FALSE ),
+                     
+                     hr(),
+                     fluidRow(column(3, verbatimTextOutput("value"))),
+                     
+                     selectInput("var", 
+                                 label = "Choose artist to show his songs",
+                                 choices = choices,
+                                 selected = "Arctic Monkeys"),      
                 dateInput('date',
                           label = 'First Available Date',
                           value = min(data$endTime)
@@ -30,7 +43,7 @@ ui <- fluidPage(
         )),
         
         mainPanel(
-            plotOutput("distPlot",height="500px")
+            plotOutput("distPlot")
         )
     )
 )
@@ -40,11 +53,25 @@ server <- function(input, output) {
 
     output$distPlot <- renderPlot({
         data1 <- data %>% filter(endTime>= input$date, endTime <= input$date2)
-        x <- data1 %>% group_by(artistName) %>% summarise(time = sum(msPlayed)) %>% arrange(desc(time)) %>% slice(1:10)
-        ggplot(x, aes(x=artistName, y=time)) + 
-            geom_bar(stat="identity", width=.5,fill="#1D428A")+theme_minimal()+
-            theme(axis.text.x = element_text(angle = 45, hjust = 1,size=13),axis.text.y =element_text(size=15))+
-            xlab(element_blank())+ylab("Hours")
+        
+        
+        if(input$checkbox==TRUE){
+            x <- data1 %>% filter(artistName == input$var) %>% group_by(song = trackName) %>% 
+                summarise(time = sum(msPlayed)) %>% 
+                arrange(desc(time)) %>% slice(1:10)
+            ggplot(x, aes(x=reorder(song,-time), y=time)) + 
+                geom_bar(stat="identity", width=.5,fill="#1D428A")+theme_minimal()+
+                theme(axis.text.x = element_text(angle = 45, hjust = 1,size=13),axis.text.y =element_text(size=15))+
+                xlab(element_blank())+ylab("Hours")
+        }
+        else{
+            x <- data1 %>% group_by(artistName) %>% summarise(time = sum(msPlayed)) %>% arrange(desc(time)) %>% slice(1:10)
+            ggplot(x, aes(x=reorder(artistName,-time), y=time)) + 
+                geom_bar(stat="identity", width=.5,fill="#1D428A")+theme_minimal()+
+                theme(axis.text.x = element_text(angle = 45, hjust = 1,size=13),axis.text.y =element_text(size=15))+
+                xlab(element_blank())+ylab("Hours")
+        }
+     
 
     })
 }
